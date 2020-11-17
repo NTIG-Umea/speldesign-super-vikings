7e19ac68061d6efdf732d494938e1919063dc6b2
Malte Klüft <malte.kluft@gmail.com>
2020-11-17 10:44:13 +0100

    Dev server setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,52 @@
 /* eslint-disable no-undef */
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-const config = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
-            {
-                test: /\.svg$/,
-                use: 'file-loader',
-            },
-            {
-                test: /\.png$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            mimetype: 'image/png',
-                        },
-                    },
-                ],
-            },
-        ],
-    },
-};
+module.exports = (env) => {
+    if (env == undefined) env = { production: false };
 
-module.exports = config;
+    const config = {
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(
+                __dirname + '/dist',
+                env.production ? 'prod' : 'dev'
+            ),
+            filename: 'bundle.js',
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/,
+                    use: ['style-loader', 'css-loader', 'sass-loader'],
+                },
+                {
+                    test: /\.svg$/,
+                    use: 'file-loader',
+                },
+                {
+                    test: /\.png$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                mimetype: 'image/png',
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+        plugins: [],
+    };
+
+    if (!env.production) {
+        config.plugins.push(
+            new HtmlWebpackPlugin({
+                title: 'Super-Vikings Development Endpoint',
+            })
+        );
+    }
+    return config;
+};
