0a4618528681d1908cbc8e68deee79432b426312
Mafrans <malte.kluft@gmail.com>
2020-12-10 09:28:40 +0100

    feat: offset each image slightly

--- src/background.js
+++ src/background.js
@@ -1,9 +1,8 @@
 import background1 from '../assets/image/background-1.png';
 import background2 from '../assets/image/background-2.png';
 import background3 from '../assets/image/background-3.png';
-import background4 from '../assets/image/background-4.png';
 
-const layers = [background1, background2, background3, background4];
+const layers = [background1, background2, background3];
 
 export default {
     preload(phaser) {
@@ -14,7 +13,13 @@ export default {
 
     create(phaser) {
         layers.forEach((layer, i) => {
-            layers[i] = phaser.add.tileSprite(400, 300, 800, 600, layer);
+            layers[i] = phaser.add.tileSprite(
+                400,
+                400 - (150 / layers.length) * (layers.length - i),
+                800,
+                600,
+                layer
+            );
         });
     },
 
