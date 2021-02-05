c8b7955aecad5c0c0ba4b3fb70678b1933654ae2
Malte Klüft <malte.kluft@gmail.com>
2021-02-05 09:24:44 +0100

    feat: adds hiscore and axios

---
+++ src/Hiscore.js
@@ -0,0 +1,44 @@
+import Axios from 'axios';
+
+export default class Hiscore {
+    // klassen behöver instantieras med en url
+    // för att testa så anv. du localhost
+    // vi kommer behöva en färdig url vid ett senare tillfälle
+    constructor(url) {
+        this.instance = Axios.create({
+            baseURL: url,
+        });
+    }
+
+    // Använd postscore när spelarens score ska postas
+    // ni kommer att få gameids, troligast gruppnamn
+    postScore(gameID, score, name) {
+        this.instance
+            .post('/hiscore', {
+                id: gameID,
+                score: score,
+                name: name.substring(0, 3),
+            })
+            .then(function (response) {
+                console.log(response.data.msg);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    // för att hämta alla hiscores så använder ni getscore
+    getScore(gameID) {
+        this.instance
+            .get('/hiscore/' + gameID)
+            .then(function (response) {
+                // handle success
+                console.log(response);
+                return response;
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            });
+    }
+}
